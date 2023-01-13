1b89b9f9dda40ea6983c89a044f8528d6ea0b543
Sander Haug <sander_haug@hotmail.com>
2023-01-13 14:12:06 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sander Haug on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
